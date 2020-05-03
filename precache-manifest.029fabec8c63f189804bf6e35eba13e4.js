c431487df4978f194134557c9801a66a3a1b1d63
akashz19 <anand.btech.cs18@iiitranchi.ac.in>
2020-05-04 03:33:21 +0530

    Updates

---
+++ precache-manifest.029fabec8c63f189804bf6e35eba13e4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a441d1092333fd081e491c5901d4982",
+    "url": "/corona/index.html"
+  },
+  {
+    "revision": "fef94a4a91788e8ad422",
+    "url": "/corona/static/css/main.c0c58e3e.chunk.css"
+  },
+  {
+    "revision": "f7ed92a11ee18a1362b0",
+    "url": "/corona/static/js/2.86c8a808.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/corona/static/js/2.86c8a808.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fef94a4a91788e8ad422",
+    "url": "/corona/static/js/main.a321e071.chunk.js"
+  },
+  {
+    "revision": "b711712299ab2066e5bf",
+    "url": "/corona/static/js/runtime-main.c1a05405.js"
+  }
+]);
